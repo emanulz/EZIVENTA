6ec457de71767b9a8a6adb8242302b9de125e934
Emanuel Zúñiga Infante <emanuzuniga@gmail.com>
2016-06-17 20:18:00 -0600

    0.0.6 Edited Django Admin templates
    Edited Django Admin templates

--- eziventa/clients/admin.py
+++ eziventa/clients/admin.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
+from.models import Client
 
 # Register your models here.
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'client_last_name', )
+
--- eziventa/clients/models.py
+++ eziventa/clients/models.py
@@ -1,5 +1,40 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+
+class Client(models.Model):
+
+    SinId = 'noid'
+    Nacional = 'naci'
+    Passaporte = 'pass'
+    Juridica = 'juri'
+
+    ID_CHOICES = ((SinId,'Sin Identificación'),
+                  (Nacional,'Cédula Nacional'),
+                  (Passaporte,'Número de Pasaporte'),
+                  (Juridica,'Cédula Jurídica')
+                  )
+
+    client_name = models.CharField(max_length=255, verbose_name='Nombre')
+    client_last_name = models.CharField(max_length=255, blank=True, verbose_name='Apellidos')
+    client_id_type = models.CharField(max_length=4, choices=ID_CHOICES, default=SinId,
+                                      verbose_name='Tipo de Identificación')
+    client_id = models.PositiveIntegerField(blank=True, verbose_name='Identificación')
+    client_address = models.CharField(max_length=255, blank=True, verbose_name='Dirección')
+    client_email = models.EmailField(verbose_name='Email', blank=True)
+    client_phone_number = models.DecimalField(max_digits=8, decimal_places=0, blank=True, verbose_name='Teléfono')
+
+    def __unicode__(self):
+        return '%s %s' % (self.client_name, self.client_last_name)
+
+    class Meta:
+
+        ordering = ['id']
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+
